d3bed172f6fbb7d1b3fac815319db438970096e2
skyler marquis <skylerjmarquis@gmail.com>
2024-01-31 23:36:14 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResponsiveWebApp;
+using ResponsiveWebApp.Models;
+
+namespace ResponsiveWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CalculateAge(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                int age = user.AgeThisYear();
+                ViewBag.Result = $"Hi, {user.Name}! You will be {age} years old on December 31st of this year.";
+
+            }
+            return View("Index");
+        }
+
+        public IActionResult ClearInputs()
+        {
+            ModelState.Clear();
+            return View("Index");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace ResponsiveWebApp.Models
+{
+    public class User
+    {
+        public string Name { get; set; } = string.Empty;
+        public int BirthYear { get; set; }
+
+        public int AgeThisYear()
+        {
+            int currentYear = DateTime.Now.Year;
+            return currentYear - BirthYear;
+        }
+    }
+}
